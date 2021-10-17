652608fce90e5e4533724acc8f9e3e091f00fe41
Ketaki230799 <85231832+Ketaki230799@users.noreply.github.com>
2021-10-17 17:56:25 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,5 +5,15 @@ class StringCalculator {
     public int add(String input) {
         return 0;
     }
+    private int getSum(String[] numbers) {
+        int sum = 0;
+        for (String currentNumber:numbers) {
+            if (stringToInt(currentNumber) > 1000) {
+                continue;
+            }
+            sum += stringToInt(currentNumber);
+        }
+        return sum;
+    }
 
 }
