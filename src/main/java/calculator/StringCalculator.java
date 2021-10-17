8492cb8d57b5b40247534e46f2113ffbae5496a9
Ketaki230799 <85231832+Ketaki230799@users.noreply.github.com>
2021-10-17 17:58:45 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,9 +2,16 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
+     public int add(String input) {
+        String[]numbers=input.split(",/\n");
+        if(input.isEmpty()){
+            return 0;
+        }else if(numbers.length>1){
+            return getSum(numbers);
+        }
+        return stringToInt(input);
     }
+    
     private int getSum(String[] numbers) {
         int sum = 0;
         for (String currentNumber:numbers) {
